a0e3703ebdecbebef814fcaadc4ba2ef9f706b95
Ehsan <ehsan.hsnzd@gmail.com>
2020-09-15 21:41:27 +0430

    add db export

--- dbseed.php
+++ dbseed.php
@@ -2,33 +2,165 @@
 require 'bootstrap.php';
 
 $statement = <<<EOS
-    CREATE TABLE IF NOT EXISTS person (
-        id INT NOT NULL AUTO_INCREMENT,
-        firstname VARCHAR(100) NOT NULL,
-        lastname VARCHAR(100) NOT NULL,
-        firstparent_id INT DEFAULT NULL,
-        secondparent_id INT DEFAULT NULL,
-        PRIMARY KEY (id),
-        FOREIGN KEY (firstparent_id)
-            REFERENCES person(id)
-            ON DELETE SET NULL,
-        FOREIGN KEY (secondparent_id)
-            REFERENCES person(id)
-            ON DELETE SET NULL
-    ) ENGINE=INNODB;
+-- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: exchange
+-- ------------------------------------------------------
+-- Server version	8.0.21
 
-    INSERT INTO person
-        (id, firstname, lastname, firstparent_id, secondparent_id)
-    VALUES
-        (1, 'Krasimir', 'Hristozov', null, null),
-        (2, 'Maria', 'Hristozova', null, null),
-        (3, 'Masha', 'Hristozova', 1, 2),
-        (4, 'Jane', 'Smith', null, null),
-        (5, 'John', 'Smith', null, null),
-        (6, 'Richard', 'Smith', 4, 5),
-        (7, 'Donna', 'Smith', 4, 5),
-        (8, 'Josh', 'Harrelson', null, null),
-        (9, 'Anna', 'Harrelson', 7, 8);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `buys`
+--
+
+DROP TABLE IF EXISTS `buys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `buys` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `amount` int DEFAULT NULL,
+  `confirm` tinyint(1) DEFAULT '0',
+  `currency_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `buys___fk_currency` (`currency_id`),
+  CONSTRAINT `buys___fk_currency` FOREIGN KEY (`currency_id`) REFERENCES `currencies` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buys`
+--
+
+LOCK TABLES `buys` WRITE;
+/*!40000 ALTER TABLE `buys` DISABLE KEYS */;
+/*!40000 ALTER TABLE `buys` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `currencies`
+--
+
+DROP TABLE IF EXISTS `currencies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `currencies` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) DEFAULT NULL,
+  `title` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `currencies`
+--
+
+LOCK TABLES `currencies` WRITE;
+/*!40000 ALTER TABLE `currencies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `currencies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `deposits`
+--
+
+DROP TABLE IF EXISTS `deposits`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `deposits` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `currency_id` int DEFAULT NULL,
+  `balance` decimal(12,8) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `deposits___fk_currency` (`currency_id`),
+  CONSTRAINT `deposits___fk_currency` FOREIGN KEY (`currency_id`) REFERENCES `currencies` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `deposits`
+--
+
+LOCK TABLES `deposits` WRITE;
+/*!40000 ALTER TABLE `deposits` DISABLE KEYS */;
+/*!40000 ALTER TABLE `deposits` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sells`
+--
+
+DROP TABLE IF EXISTS `sells`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sells` (
+  `id` int DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  `confirm` tinyint DEFAULT NULL,
+  `currency_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sells`
+--
+
+LOCK TABLES `sells` WRITE;
+/*!40000 ALTER TABLE `sells` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sells` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `mobile` varchar(32) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_pk` (`email`),
+  UNIQUE KEY `users_pk_2` (`mobile`)
+) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'ehsan','ehsan@gmail.com',NULL,'09125126492',NULL,NULL),(87,'ehsan','ehsasn@gmail.com','123','09125126491','2020-09-15 15:10:50','2020-09-15 15:10:50'),(88,'ehsan','ehsan3@gmail.com','202cb962ac59075b964b07152d234b70','09125126493','2020-09-15 15:13:03','2020-09-15 15:13:03'),(89,'ehsan','ehsan4@gmail.com','eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMTIzIn0.w7PpX6w0gaZCjCZe2Q_WpTDOqZtcz8ZY0pxnYiLiq_c','09125126494','2020-09-15 15:34:36','2020-09-15 15:34:36');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-15 21:40:19
 EOS;
 
 try {
